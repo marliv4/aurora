27a3620e4ddd3113b63f50c9fb7caa5a573638cb
Marko Livajusic <marko_livajusic0@protonmail.com>
2024-04-28 12:13:21 +0200

    [CreateModule]: add Three.js for rendering!

--- src/main/resources/static/js/create.js
+++ src/main/resources/static/js/create.js
@@ -1,112 +1,40 @@
-const canvas = document.querySelector("#glcanvas");
-const gl = canvas.getContext("webgl");
+import * as THREE from 'three';
 
-function renderImg() {
-    const fileInput = document.getElementById("file");
-    const file = fileInput.files[0];
-
-    if (!file) {
-        console.error("No file selected.");
-        return;
-    }
-
-    const reader = new FileReader();
-    reader.onload = function(event) {
-        const image = new Image();
-        image.onload = function() {
-        };
-        image.src = event.target.result;
-    };
-    reader.readAsDataURL(file);
+function uploadImg() {
+    const fileInput = document.getElementById("file").value;
+    alert(fileInput);
 }
 
-document.getElementById("uploadButton").addEventListener("click", renderImg);
 
-if (gl === null) {
-    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+document.getElementById("uploadButton").addEventListener("click", uploadImg);
+
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+camera.position.z = 5;
+
+const geometry = new THREE.BoxGeometry();
+
+const material = new  THREE.MeshBasicMaterial({color: 0x00ff00});
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
 }
 
-var vertices = [
-    -1.0,  1.0, 0.0,
-    -1.0, -1.0, 0.0,
-    1.0, -1.0, 0.0,
-    1.0,  1.0, 0.0
-];
-
-var indices = [
-    0, 1, 2,
-    0, 2, 3
-];
-
-function createVertexBuffer(vertices){
-    var vertex_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-    return vertex_buffer;
-}
-
-function createIndexBuffer(indices) {
-    var index_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-
-    return index_buffer;
-}
-
-function createShader(vertex, vertCode) {
-    var shader = 0;
-    if (vertex) {
-        shader = gl.createShader(gl.VERTEX_SHADER);
-    } else {
-        shader = gl.createShader(gl.FRAGMENT_SHADER);
-    }
-
-    gl.shaderSource(shader, vertCode);
-    gl.compileShader(shader);
-
-    return shader;
-}
-
-function createProgram(vertex, fragment) {
-    var program = gl.createProgram();
-    gl.attachShader(program, vertex);
-    gl.attachShader(program, fragment);
-    gl.linkProgram(program);
-    gl.useProgram(program);
-
-    return program;
-}
-
-var vertCode =
-    'attribute vec3 coordinates;' +
-    'void main(void) {' +
-    ' gl_Position = vec4(coordinates, 1.0);' +
-    '}';
-
-
-var fragCode =
-    'void main(void) {' +
-    ' gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);' +
-    '}';
-
-var vertShader = createShader(true, vertCode);
-var fragShader = createShader(false, fragCode);
-var shaderProgram = createProgram(vertShader, fragShader);
-
-var vertex_buffer = createVertexBuffer(vertices);
-var index_buffer = createIndexBuffer(indices);
-
-gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-var coord = gl.getAttribLocation(shaderProgram, "coordinates");
-gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0);
-gl.enableVertexAttribArray(coord);
-
-gl.clearColor(0.35, 0.35, 0.35, 1.0);
-gl.clear(gl.COLOR_BUFFER_BIT);
-gl.viewport(0.0, 0.0, canvas.width, canvas.height);
-
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+animate();
