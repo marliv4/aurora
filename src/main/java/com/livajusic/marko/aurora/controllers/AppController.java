35c4973a45635cc69410e4c568b045a47a35c118
Marko Livajusic <marko_livajusic0@protonmail.com>
2024-05-18 11:29:29 +0200

    [Index]: display GIFs and author information

--- src/main/java/com/livajusic/marko/aurora/controllers/AppController.java
+++ src/main/java/com/livajusic/marko/aurora/controllers/AppController.java
@@ -1,12 +1,19 @@
 package com.livajusic.marko.aurora.controllers;
 
 import com.livajusic.marko.aurora.db_repos.GifRepo;
+import com.livajusic.marko.aurora.db_repos.UserRepo;
 import com.livajusic.marko.aurora.tables.AuroraGIF;
+import com.livajusic.marko.aurora.tables.AuroraUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Controller
@@ -15,20 +22,28 @@ public class AppController {
 
     private final GifRepo gifRepo;
 
-    public AppController(GifRepo gifRepo) {
+    @Autowired
+    private final UserRepo userRepo;
+
+    public AppController(GifRepo gifRepo,
+                         UserRepo userRepo) {
         this.gifRepo = gifRepo;
+        this.userRepo = userRepo;
     }
 
     @GetMapping(path = {"/", "/index"})
     public String index(Model model) {
         List<AuroraGIF> allGifs = gifRepo.findAll();
-        List<String> paths = allGifs.stream().map(AuroraGIF::getPath).collect(Collectors.toList());
-
-        for (String path : paths) {
-            System.out.println("path: " + path);
+        List<Map<String, Object>> gifAttributes = new ArrayList<>();
+        for (AuroraGIF gif : allGifs) {
+            Map<String, Object> gifData = new HashMap<>();
+            gifData.put("path", gif.getPath());
+            gifData.put("author", gif.getUser().getUsername());
+            gifData.put("date", gif.getPublishDate());
+            gifData.put("license", gif.getLicence());
+            gifAttributes.add(gifData);
         }
-
-        model.addAttribute("gifPaths", paths);
+        model.addAttribute("gifs", gifAttributes);
 
         return "index";
     }
