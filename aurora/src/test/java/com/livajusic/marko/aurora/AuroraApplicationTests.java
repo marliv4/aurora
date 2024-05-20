248791db570f08810f19a0bb67c5cc3a53bdb0e4
Marko Livajusic <marko_livajusic0@protonmail.com>
2024-05-20 07:53:57 +0200

    [General]: Import resources & test folder

---
+++ aurora/src/test/java/com/livajusic/marko/aurora/AuroraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livajusic.marko.aurora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuroraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
